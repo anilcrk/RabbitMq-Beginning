e87d68f60a4fe90b5659734279db6282ec6f7417
Anil <anilcirik37@gmail.com>
2021-06-07 01:15:19 +0300

    Add project files.

---
+++ UdemyRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,59 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace UdemyRabbitMQ.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://iilgwgrl:ezp3WGQ8F8xoF4bqnhlsTWMKz8PMwmJV@baboon.rmq.cloudamqp.com/iilgwgrl");
+
+            using var connection = factory.CreateConnection(); // open connection
+
+            var channel = connection.CreateModel(); // created channel
+
+            /*
+             * durable --> false ise rabbitmq de oluşan kuyruklar memoryde tutulur.
+            true ise kuyruklar fiziksel olarak kayıt edilir restrat atılsa bile tutulur.
+            */
+
+            /*
+             excslusive true ise bu kuyruğa sadece bu kadal üzerinden bağlanılır.
+             farklı bir kanal üzerinden bağlanılacak için false yapıyoruz.
+             */
+
+            /*
+             autodelete : Bütün subcriber lar giderse kutruk silinir.
+             otomatik silinmemesi için false yapıyoruz.
+             */
+
+            //channel.QueueDeclare("hello-queue", true, false, false);
+
+            channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                // RabbitMq mesajlar byte dizi olarak gider bu sebeple her türlü tipte veri gönderilir.
+                string message = $"Log {x}"; // created message
+
+                var messageBody = Encoding.UTF8.GetBytes(message); //message Convert to byte array
+
+                /*
+                 Direk kuyruğa mesajı gönderdiğimiz için exchange string.empty gönderildi.
+                 Default exchange kullanmak için routingKey property sine mutlaka kuyruk ismi verilir.
+                 */
+                channel.BasicPublish("logs-fanout", "", null, messageBody); ;
+
+                Console.Write($"Mesaj gönderildi. Mesaj : {message}");
+            });
+
+           
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UdemyRabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,52 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace UdemyRabbitMQ.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://iilgwgrl:ezp3WGQ8F8xoF4bqnhlsTWMKz8PMwmJV@baboon.rmq.cloudamqp.com/iilgwgrl");
+
+            using var connection = factory.CreateConnection(); // open connection
+
+            var channel = connection.CreateModel(); // created channel
+
+            var randomQueueName = channel.QueueDeclare().QueueName; // random queue name
+            channel.QueueBind(randomQueueName, "logs-fanout", "", null);
+
+            // açıklamar publisher içinde
+            //channel.QueueDeclare("hello-queue", true, false, false);
+
+            channel.BasicQos(0, 1, false);
+            var consumer = new EventingBasicConsumer(channel);
+
+            /*
+             autoAck --> true verilirse rabbitmq subscriber'a bir mesaj gönderildiğinde mesaj başarılı veya başarısız da işlense siler.
+             
+             */
+            channel.BasicConsume(randomQueueName, false, consumer);
+
+            Console.WriteLine("Loglar dinleniyor...");
+
+            // rabbitmq subscriber mesaj gönderidiğinde event çalışır.
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Thread.Sleep(1500);
+
+                Console.WriteLine($"Gelen Mesaj : {message}");
+
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
